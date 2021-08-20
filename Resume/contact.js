e8c408a011f1af1cad74b79502492b67e1b9d5b6
Obonganyanga Joshua <79257899+hobeeJ@users.noreply.github.com>
2021-08-21 00:17:08 +0100

    Initial Commit

---
+++ Resume/contact.js
@@ -0,0 +1,13 @@
+// Pop Up
+
+document.getElementById('button').addEventListener('click', 
+function() {
+    document.querySelector('.bg-modal').style.display = 'block';
+});
+
+
+document.querySelector('.btn2').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
